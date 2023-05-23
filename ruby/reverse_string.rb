ca1d7fcfa338aaf217a69b6bc0edc23ec8ca5d84
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-05-23 16:08:45 -0400

    completed JS and ruby

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,15 +1,25 @@
 def reverse_string(str)
   # type your code in here
+  result_arr = []
+  string_arr = str.split("")
+  len = str.size
+
+  string_arr.each_with_index do |char, index|
+    new_index = len - index
+    result_arr[new_index] = char
+  end
+
+  result_arr.join
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
-  puts "=>", reverse_string('hi')
+  puts "=>", reverse_string("hi")
 
   puts
 
   puts "Expecting: 'ybabtac'"
-  puts "=>", reverse_string('catbaby')
+  puts "=>", reverse_string("catbaby")
 
   # Don't forget to add your own!
 end
